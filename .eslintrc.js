c5909ed7e304597b8c1aab6d3c58fa5268754746
changzhn <changzhn@yonyou.com>
2018-03-24 13:05:24 +0800

    修改了一些配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  env: {
-    browser: true,
-  },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential'],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  }
-}
+	root: true,
+	parserOptions: {
+		parser: "babel-eslint"
+	},
+	env: {
+		browser: true
+	},
+	// https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+	// consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+	extends: ["plugin:vue/essential"],
+	// required to lint *.vue files
+	plugins: ["vue"],
+	// add your custom rules here
+	rules: {
+		// allow debugger during development
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		indent: ["warn", "tab"],
+		"linebreak-style": ["warn", "unix"],
+		quotes: ["warn", "single"],
+		semi: ["warn", "always"],
+		"no-unused-vars": ["off"],
+		"no-console": ["error"],
+		"no-undef": ["off"]
+	}
+};
